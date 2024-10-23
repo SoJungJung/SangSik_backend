777091b8bc03641ea55b41fff37d1a41d1cd23cf
이준형 <gaejungrhee@ijunhyeong-ui-MacBookPro.local>
2024-10-23 11:57:40 +0900

    port number

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3001;
 require('dotenv').config(); // .env 파일에 정의된 환경변수 로드
 
 const { Pool } = require('pg');
